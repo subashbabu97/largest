4bbbe855d44bc01619a5d97f6e11877ed1e01802
subashbabu97 <48273911+subashbabu97@users.noreply.github.com>
2019-06-06 11:57:53 +0530

    Create among 3 numbers.py

---
+++ among 3 numbers.py
@@ -0,0 +1,15 @@
+a=int(input("enter the 1st number))
+b=int(input("enter the 2nd number))
+c=int(input("enter the 3rd number))
+if a>b:
+    if a>c:
+        print(a)
+    else:
+        print(c)
+else:
+    if b>c:
+        print(b)
+    else:
+        print(c)
+     
+     
